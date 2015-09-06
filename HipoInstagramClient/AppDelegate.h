232b5b24d1611d5ee939a574c41187bc7282deb5
Ahmet Karalar <akaralar@Ahmets-MacBook-Pro.local>
2015-09-06 17:58:25 +0300

    initial commit

---
+++ HipoInstagramClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HipoInstagramClient
+//
+//  Created by Ahmet Karalar on 06/09/15.
+//  Copyright (c) 2015 akaralar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HipoInstagramClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HipoInstagramClient
+//
+//  Created by Ahmet Karalar on 06/09/15.
+//  Copyright (c) 2015 akaralar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
